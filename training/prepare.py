a73ea22cda2cfac92ac83c96a0510340ac767119
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2023-11-16 09:44:57 -0500

    Fix data prep (fixes #1)

--- training/prepare.py
+++ training/prepare.py
@@ -1,4 +1,4 @@
 from datasets import load_dataset
-dataset = load_dataset("mosaicml/instruct-v3")
+dataset = load_dataset("mosaicml/instruct-v3", split="train")
 dataset = dataset.filter(lambda x: x["source"] != "dolly_hhrlhf")
-dataset.save_to_disk("./instruct-data")
+dataset.to_json("./instruct-data/train.jsonl")
